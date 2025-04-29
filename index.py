fdab62055fc0614d06f2e03954e77d0439163f6d
Gabriel leite <84857164+gabrielleite12@users.noreply.github.com>
2025-04-29 11:19:16 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,5 +1,3 @@
 # api/index.py
-from flask_app import app as application  # ou o nome do seu app
+from flask_app import app as app # ou o nome do seu app
 
-def handler(event, context):
-    return application(event, context)
